e8835d8fe6e5a0ba73546c2165dbad0bebf23a68
hebervictor <heber.pedro@br.experian.com>
2021-11-01 15:00:01 -0300

    feat: cart page

---
+++ src/pages/Cart/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import * as S from './styles'
+
+const Cart = () => {
+  return (
+    <S.Container>
+      <S.Title>Cart Page</S.Title>
+    </S.Container>
+  )
+}
+
+export default Cart
