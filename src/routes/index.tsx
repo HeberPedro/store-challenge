74e141f7df95a68954600ce2f83bc5dbb5aa0c3d
hebervictor <heber.pedro@br.experian.com>
2021-11-01 15:00:56 -0300

    feat: update routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,18 +2,26 @@ import React from 'react'
 
 import { createStackNavigator } from '@react-navigation/stack'
 
+import Cart from '../pages/Cart'
 import Home from '../pages/Home'
 
-const App = createStackNavigator()
+export type RootStackParamList = {
+  Home: undefined
+  Cart: undefined
+}
+
+const App = createStackNavigator<RootStackParamList>()
 
 const Routes = () => (
   <App.Navigator
+    initialRouteName="Home"
     screenOptions={{
       headerShown: false,
       cardStyle: { backgroundColor: '#312e38' },
     }}
   >
     <App.Screen name="Home" component={Home} />
+    <App.Screen name="Cart" component={Cart} />
   </App.Navigator>
 )
 
