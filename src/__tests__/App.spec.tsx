64c632a5e7961fe3dc788104274ed018886e3dda
heberpedro <heberv.pedro@gmail.com>
2021-11-02 15:10:54 -0300

    test: app root

---
+++ src/__tests__/App.spec.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import App from '../App'
+import { render } from './test-utils'
+
+describe('APP', () => {
+  test('should render App', () => {
+    render(<App />)
+  })
+})
